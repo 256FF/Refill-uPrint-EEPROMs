3d0d7d65d9e1ce061ce290f6ba1ad41903e9b152
Odin <256FF@users.noreply.github.com>
2017-03-28 07:34:33 +0200

    Update bat
    Translated to english

--- flip + generate + clipboard.bat
+++ flip + generate + clipboard.bat
@@ -13,18 +13,19 @@ set HX_5=%8
 set HX_6=%9
 set HX_7=%10
 
-:: Eingabe der Werte
+:: Input
 set /p UID=UID eingeben: 
 
-:: Seriennummer einzugeben
-:: set /p SNR=Seriennummer eingeben (x.0): 
+:: Option to manually input serial number
+:: set /p SNR=New serial number (x.0): 
 
 :: Seriennummer automatisch erzeugen aus Datum und Uhrzeit
-:: JJJJMMTTMMSS - ohne HH
+:: Create serial number with time and date
+:: JJMMTTMMSS - without HH
 :: set /a SNR=%date:~8,2%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%
 set /a SNR=%date:~8,2%%date:~3,2%%date:~0,2%%time:~3,2%%time:~6,2%
 
-:: UID umkehren (swap)
+:: UID umkehren / swap UID
 set HX_0=%UID:~0,2%
 set HX_1=%UID:~3,2%
 set HX_2=%UID:~6,2%
@@ -36,28 +37,28 @@ set HX_7=%UID:~21,2%
 set UID=%HX_7%%HX_6%%HX_5%%HX_4%%HX_3%%HX_2%%HX_1%%HX_0%
 
 :: Ausgabe der eingegebenen Werte
+:: Show the values
 echo\
-echo Ausgabe zur Kontrolle:
+echo Used values:
 echo SwapUID: %UID%
 echo New SNr: %SNR%
 echo\
-echo\
 
-:: Wechsel in das Verzeichnis und erstellen der new.bin
-:: cd C:\Temp\refill\
+:: Erstellen der new.bin mit bevanheu's script
+:: Create the new *.bin file with bvanheu's script
 stratasys-cli.py %VAR_1% %UID% %VAR_3% %SNR% %VAR_4%
 
 :: Show info of the .bin file
 stratasys-cli.py %VAR_1% %UID% -i new.bin
-echo\
-echo\
 
 :: Umwandeln der new.bin in CSV + CopyData2Clipboard
+:: Convert the new.bin into CSV and copy the data to the clipboard with write-command for the DIAG-Port
 set VAR_1=-i new.bin -s -o csv.txt
 ::cd C:\Temp\refill\
 trans_cp.py %VAR_1%
 
 echo\
-echo Die Werte wurden in die Zwischenablage kopiert.
+echo Write comand has been copied to the clipboard. Swtich to putty to insert it. 
+echo Make sure to choose the right bay! 
 echo\
 pause
